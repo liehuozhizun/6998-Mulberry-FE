f51021f3f779b415d1fca20ade7dd6119f3a5d45
yuxuanshi <yuxuan.shi@vanderbilt.edu>
2023-04-09 20:19:21 -0400

    sign-in page update

--- src/components/shared.js
+++ src/components/shared.js
@@ -7,33 +7,8 @@ export const COLORS = {
     PINK_T: "#F5EAEA",
     RED_T: "#F16767",
     ORANGE_T: "#FFB84C",
-    WHITE_T: "#FFFFFF"
+    WHITE_T: "#FFFFFF",
+    GREY_T: "#D9D9D9"
 };
-
-
-// Example of prop-type usage, could be helpful later as well
-// export const ErrorMessage = ({msg = "", hide = false}) => {
-//     return (
-//         <ErrorBase style={{display: hide ? "none" : "inherit"}}>{msg}</ErrorBase>
-//     );
-// };
-
-// ErrorMessage.propTypes = {
-//     msg: PropTypes.string,
-//     hide: PropTypes.bool,
-// };
-
-// Example of a styled component
-// TODO: rename or delete later
-export const Button = styled.button`
-  max-width: 200px;
-  min-width: 150px;
-  max-height: 2em;
-  background: #6495ed;
-  border: none;
-  border-radius: 5px;
-  line-height: 2em;
-  font-size: 0.8em;
-`;
 
 
